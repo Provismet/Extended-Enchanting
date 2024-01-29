b55451cb647ebe0fa274dd14adc1b327d1160820
Provismet <17149901+Provismet@users.noreply.github.com>
2024-01-29 19:31:47 +0000

    Fix utility enchantments.

--- src/main/java/com/provismet/ExtendedEnchanting/enchantments/BackFootEnchantment.java
+++ src/main/java/com/provismet/ExtendedEnchanting/enchantments/BackFootEnchantment.java
@@ -4,27 +4,41 @@ import com.provismet.CombatPlusCore.enchantments.OffHandEnchantment;
 import com.provismet.CombatPlusCore.enchantments.WeaponUtilityEnchantment;
 
 import net.minecraft.enchantment.Enchantment;
+import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.util.math.Vec3d;
 
 public class BackFootEnchantment extends WeaponUtilityEnchantment {
     public BackFootEnchantment () {
-        super(Rarity.UNCOMMON, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND);
+        super(Rarity.UNCOMMON, EnchantmentTarget.WEAPON, EquipmentSlot.MAINHAND, EquipmentSlot.OFFHAND);
     }
 
     @Override
     public void postChargedHit (int level, LivingEntity user, LivingEntity target) {
         super.postChargedHit(level, user, target);
-        if (!user.getWorld().isClient()) {
-            double dx = target.getX() - user.getX();
-            double dz = target.getZ() - user.getZ();
-            user.takeKnockback(1.0, dx, dz);
-        }
+        Vec3d velocity = new Vec3d(user.getX() - target.getX(), 0.0, user.getZ() - target.getZ()).normalize().multiply(1 + (level - 1) * 0.5).add(0.0, 0.1, 0.0);
+        user.addVelocity(velocity);
     }
     
     @Override
-    protected boolean canAccept(Enchantment other) {
+    protected boolean canAccept (Enchantment other) {
         if (other instanceof OffHandEnchantment) return true;
         return super.canAccept(other);
+    }
+
+    @Override
+    public int getMaxLevel () {
+        return 2;
+    }
+
+    @Override
+    public int getMinPower (int level) {
+       return 10 + 10 * level;
+    }
+
+    @Override
+    public int getMaxPower (int level) {
+        return this.getMinPower(level) + 50;
     }
 }
--- src/main/java/com/provismet/ExtendedEnchanting/enchantments/DualStrikeEnchantment.java
+++ src/main/java/com/provismet/ExtendedEnchanting/enchantments/DualStrikeEnchantment.java
@@ -1,31 +1,58 @@
 package com.provismet.ExtendedEnchanting.enchantments;
 
 import com.provismet.CombatPlusCore.enchantments.WeaponUtilityEnchantment;
+import com.provismet.CombatPlusCore.interfaces.MeleeWeapon;
 import com.provismet.lilylib.util.Relations;
 
+import net.minecraft.enchantment.EnchantmentTarget;
 import net.minecraft.entity.EquipmentSlot;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.ai.TargetPredicate;
+import net.minecraft.entity.attribute.EntityAttributes;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.server.world.ServerWorld;
+import net.minecraft.item.ItemStack;
 
 public class DualStrikeEnchantment extends WeaponUtilityEnchantment {
     public DualStrikeEnchantment () {
-        super(Rarity.UNCOMMON, EquipmentSlot.MAINHAND);
+        super(Rarity.UNCOMMON, EnchantmentTarget.WEAPON, EquipmentSlot.MAINHAND);
     }
     
     @Override
     public void postChargedHit (int level, LivingEntity user, LivingEntity target) {
         super.postChargedHit(level, user, target);
-        if (user.getWorld() instanceof ServerWorld serverWorld) {
-            LivingEntity otherTarget = serverWorld.getClosestEntity(LivingEntity.class, TargetPredicate.createAttackable().setPredicate(entity -> {
-                return !Relations.isFriendly(user, entity);
-            }), target, target.getX(), target.getY(), target.getZ(), target.getBoundingBox().expand(2.5, 0.25, 2.5));
+        LivingEntity otherTarget = user.getWorld().getClosestEntity(LivingEntity.class, TargetPredicate.createAttackable().setPredicate(entity -> {
+            return !Relations.isFriendly(user, entity) && entity != target;
+        }), user, target.getX(), target.getY(), target.getZ(), target.getBoundingBox().expand(2.5, 0.25, 2.5));
 
-            if (otherTarget != null) {
-                if (user instanceof PlayerEntity player) target.damage(user.getDamageSources().playerAttack(player), level * 2.5f);
-                else target.damage(user.getDamageSources().mobAttack(user), level * 2.5f);
+        if (otherTarget != null) {
+            double damage = 1 + user.getAttributeValue(EntityAttributes.GENERIC_ATTACK_DAMAGE) * level * 0.2;
+            if (user instanceof PlayerEntity player) otherTarget.damage(user.getDamageSources().playerAttack(player), (float)damage);
+            else otherTarget.damage(user.getDamageSources().mobAttack(user), (float)damage);
+
+            ItemStack heldItem = user.getMainHandStack();
+            if (heldItem.getItem() instanceof MeleeWeapon melee) {
+                melee.postChargedHit(heldItem, user, otherTarget);
+
+                if (user.fallDistance > 0.0f && !user.isOnGround() && !user.isClimbing() && !user.isTouchingWater() && !user.hasStatusEffect(StatusEffects.BLINDNESS) && !user.hasVehicle() && otherTarget instanceof LivingEntity) {
+                    melee.postCriticalHit(heldItem, user, otherTarget);
+                }
             }
         }
+    }
+
+    @Override
+    public int getMaxLevel () {
+        return 3;
+    }
+
+    @Override
+    public int getMinPower (int level) {
+        return 10 + 5 * level;
+    }
+
+    @Override
+    public int getMaxPower (int level) {
+        return this.getMinLevel() + 50;
     }
 }
