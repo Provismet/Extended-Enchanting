75351ee6ea8b55852a5ba31cc2fcaa1ed75d4a4a
Provismet <17149901+Provismet@users.noreply.github.com>
2024-07-08 14:51:53 -0700

    Fix enchanted loot tables.

--- src/main/java/com/provismet/ExtendedEnchanting/ExtendedEnchantingMain.java
+++ src/main/java/com/provismet/ExtendedEnchanting/ExtendedEnchantingMain.java
@@ -1,5 +1,6 @@
 package com.provismet.ExtendedEnchanting;
 
+import com.provismet.CombatPlusCore.loot.functions.EnchantRandomlyFromKeyLootFunction;
 import com.provismet.ExtendedEnchanting.registries.EEEnchantmentComponentTypes;
 import com.provismet.ExtendedEnchanting.registries.EELambdas;
 import com.provismet.ExtendedEnchanting.registries.EESingleEntityEnchantmentEffects;
@@ -9,7 +10,6 @@ import net.minecraft.item.Items;
 import net.minecraft.loot.LootPool;
 import net.minecraft.loot.LootTables;
 import net.minecraft.loot.entry.ItemEntry;
-import net.minecraft.loot.function.EnchantRandomlyLootFunction;
 import net.minecraft.loot.provider.number.BinomialLootNumberProvider;
 import net.minecraft.util.Identifier;
 
@@ -43,54 +43,54 @@ public class ExtendedEnchantingMain implements ModInitializer {
 				if (LootTables.BASTION_TREASURE_CHEST.equals(id) || LootTables.BASTION_HOGLIN_STABLE_CHEST.equals(id)) {
 					tableBuilder.pool(
 						LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.05f))
-							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.BRIMSTONE_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.BRIMSTONE_HEART.getEntryOrThrow())))
+							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.BRIMSTONE_HEART)))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.BRIMSTONE_HEART)))
 					);
 				}
 				else if (LootTables.END_CITY_TREASURE_CHEST.equals(id)) {
 					tableBuilder.pool(
 						LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.05f))
-							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.VOID_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.VOID_HEART.getEntryOrThrow())))
+							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.VOID_HEART)))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.VOID_HEART)))
 					)
 					.pool(
 						LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.025f))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SOLITUDE.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.DIAMOND_SWORD).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SOLITUDE.getEntryOrThrow())))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SOLITUDE)))
+							.with(ItemEntry.builder(Items.DIAMOND_SWORD).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SOLITUDE)))
 					);
 				}
 				else if (LootTables.WOODLAND_MANSION_CHEST.equals(id) || LootTables.ANCIENT_CITY_CHEST.equals(id)) {
 					tableBuilder.pool(
 						LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.05f))
-							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SUN_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SUN_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.MOON_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.MOON_HEART.getEntryOrThrow())))
+							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SUN_HEART)))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SUN_HEART)))
+							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.MOON_HEART)))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.MOON_HEART)))
 					)
 					.pool(
 						LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.025f))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SOLITUDE.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.DIAMOND_SWORD).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SOLITUDE.getEntryOrThrow())))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SOLITUDE)))
+							.with(ItemEntry.builder(Items.DIAMOND_SWORD).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SOLITUDE)))
 					);
 				}
 				else if (LootTables.HERO_OF_THE_VILLAGE_ARMORER_GIFT_GAMEPLAY.equals(id)) {
 					tableBuilder.pool(
 						LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.005f))
-							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SUN_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SUN_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.MOON_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.MOON_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.BRIMSTONE_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.BRIMSTONE_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.VOID_HEART.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.VOID_HEART.getEntryOrThrow())))
+							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SUN_HEART)))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SUN_HEART)))
+							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.MOON_HEART)))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.MOON_HEART)))
+							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.BRIMSTONE_HEART)))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.BRIMSTONE_HEART)))
+							.with(ItemEntry.builder(Items.DIAMOND_CHESTPLATE).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.VOID_HEART)))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.VOID_HEART)))
 					);
 				}
 				else if (LootTables.HERO_OF_THE_VILLAGE_WEAPONSMITH_GIFT_GAMEPLAY.equals(id)) {
 					tableBuilder.pool(
 						LootPool.builder().rolls(BinomialLootNumberProvider.create(1, 0.005f))
-							.with(ItemEntry.builder(Items.BOOK).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SOLITUDE.getEntryOrThrow())))
-							.with(ItemEntry.builder(Items.DIAMOND_SWORD).apply(new EnchantRandomlyLootFunction.Builder().option(EEEnchantments.SOLITUDE.getEntryOrThrow())))
+							.with(ItemEntry.builder(Items.BOOK).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SOLITUDE)))
+							.with(ItemEntry.builder(Items.DIAMOND_SWORD).apply(EnchantRandomlyFromKeyLootFunction.create().option(EEEnchantments.SOLITUDE)))
 					);
 				}
 			}
