f7f65090ca03fd0f65cd53f111915fbc28de985f
Provismet <17149901+Provismet@users.noreply.github.com>
2024-12-06 20:13:16 +0000

    Code style.

--- src/main/java/com/provismet/ExtendedEnchanting/interfaces/IMixinLivingEntity.java
+++ src/main/java/com/provismet/ExtendedEnchanting/interfaces/IMixinLivingEntity.java
@@ -3,8 +3,8 @@ package com.provismet.ExtendedEnchanting.interfaces;
 import net.minecraft.util.math.Vec3d;
 
 public interface IMixinLivingEntity {
-    public abstract void extended_Enchanting$applyStatic (int amount);
+    void extended_Enchanting$applyStatic (int amount);
     
-    public abstract Vec3d extended_Enchanting$getPreviousGroundPos ();
-    public abstract void extended_Enchanting$setPreviousGroundPos (Vec3d position);
+    Vec3d extended_Enchanting$getPreviousGroundPos ();
+    void extended_Enchanting$setPreviousGroundPos (Vec3d position);
 }
